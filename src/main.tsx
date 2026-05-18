ba47fb920f264e5e8de12dd05bd4b219c3a59c6e
HP486379 <soffio2@gmail.com>
2026-05-18 23:02:02 +0900

    Load battlefield board skin

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App.tsx';
 import './index.css';
 import './extra.css';
 import './unitIcons.css';
+import './battlefield.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
