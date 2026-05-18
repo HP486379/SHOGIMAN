a20aeadcc849632498d5d591ef488cc029e403c2
HP486379 <soffio2@gmail.com>
2026-05-18 22:12:38 +0900

    Load military unit icon styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import './extra.css';
+import './unitIcons.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
