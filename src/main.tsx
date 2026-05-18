86a2283247e3ada83e63baeb227816ad9ca2ce78
HP486379 <soffio2@gmail.com>
2026-05-18 16:57:29 +0900

    Load extra gameplay styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import './extra.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
