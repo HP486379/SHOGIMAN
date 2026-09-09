fd306fe676b9ab396a54f5df522eea7f122d9c70
HP486379 <soffio2@gmail.com>
2026-09-10 07:47:59 +0900

    Load display-mode UNIT GUIDE styles

--- src/main.tsx
+++ src/main.tsx
@@ -10,6 +10,7 @@ import './checkEffects.css';
 import './uxFixes.css';
 import './aiAdvisor.css';
 import './hybridReadability.css';
+import './unitGuideModes.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
